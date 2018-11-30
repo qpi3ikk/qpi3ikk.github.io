219f6c3c6a4679c9ead2bc370d93629c3c73760a
qpi3ikk <qpi3ikk@gmail.com>
2018-11-30 11:38:54 +0200

    1.0
    1.0

---
+++ f-real-psd/js/main.js
@@ -0,0 +1,48 @@
+// Плаваюче меню
+(function($) { 
+    "use strict";
+
+    var $navbar = $("#navbar"),
+        y_pos = $navbar.offset().top,
+        height = $navbar.height();
+
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+
+        if (scrollTop > y_pos + height) {
+            $navbar.addClass("navbar-fixed").animate({
+                top: 0
+            });
+        } else if (scrollTop <= y_pos) {
+            $navbar.removeClass("navbar-fixed").clearQueue().animate({
+                top: "-48px"
+            }, 0);
+        }
+    });
+
+})(jQuery, undefined);
+
+// кнопка вверх
+$(function() {
+ 
+    $(window).scroll(function() {
+     
+    if($(this).scrollTop() != 0) {
+     
+    $('#toTop').fadeIn();
+     
+    } else {
+     
+    $('#toTop').fadeOut();
+     
+    }
+     
+    });
+     
+    $('#toTop').click(function() {
+     
+    $('body,html').animate({scrollTop:0},800);
+     
+    });
+     
+});
