a2c69a9596ec11fd7c556ac5e0665c6dad02e9f8
qpi3ikk <qpi3ikk@gmail.com>
2018-12-02 19:35:35 +0200

    1.2
    1.2

--- f-real-psd/js/main.js
+++ f-real-psd/js/main.js
@@ -46,3 +46,35 @@ $(function() {
     });
      
 });
+
+// Слайдер
+
+jQuery(function($){
+
+    $(document).ready(function(){
+        $('.slickslider').slick({
+            centerPadding: '60px',
+            slidesToShow: 3,
+            responsive: [
+                {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+                },
+                {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+                }
+            ]
+        });
+    });
+});
