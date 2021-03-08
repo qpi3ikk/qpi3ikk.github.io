013cc0daf6966e629e968107810e54c1a4b74f08
qpi3ikk <44373324+qpi3ikk@users.noreply.github.com>
2021-03-08 14:40:07 +0200

    5
    5

--- RobShim/mail.php
+++ RobShim/mail.php
@@ -1,7 +1,7 @@
 <?php
 
-$recepient = "qpi3ikk@gmail.com, vitalik-cool@bigmir.net";
-$sitename = "12Bytes";
+$recepient = "shimon.r.photo@gmail.com";
+$sitename = "robert-foto";
 
 $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
@@ -9,7 +9,7 @@ $email = trim($_POST["email"]);
 $text = trim($_POST["text"]);
 $message = "Имя: $name \nТелефон: $phone \nEmail: $email \nТекст: $text";
 
-$pagetitle = "Нова заявка с сайту \"$sitename\"";
-$headers = 'From:admin@12bytes.com.ua';
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$headers = 'From:admin@robert-foto.com';
 mail($recepient, $pagetitle, $message, $headers);
 //mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
